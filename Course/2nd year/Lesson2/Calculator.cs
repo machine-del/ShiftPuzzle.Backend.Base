172fb74b07cb1613ef9cfcb2cab9db1af9aa7600
machine <137556120+machine-del@users.noreply.github.com>
2024-10-08 17:56:20 +0500

    HomeWork

---
+++ Course/2nd year/Lesson2/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        string Input = Console.ReadLine();
+        double num1 = Convert.ToDouble(Input);
+
+        Console.Write("Введите второе число: ");
+        string Input1 = Console.ReadLine();
+        double num2 = Convert.ToDouble(Input1);
+
+        double result = num1 + num2;
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
---
+++ Course/2nd year/Lesson2/Random.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int secretNum = 52;
+
+        while (true)
+        {
+            Console.Write("Введите ваше число: ");
+            string secretGuest = Console.ReadLine();
+            int guestNum = Convert.ToInt32(secretGuest);
+
+            if (secretNum == guestNum)
+            {
+                Console.WriteLine("Поздравляю! Вы отгадали число!");
+                break;
+            } else if (guestNum < secretNum)
+            {
+                Console.WriteLine("Ваше число слишком маленькое.");
+            } else
+            {
+                Console.WriteLine("Ваше число слишком большое.");
+            }
+        }
+    }
+}
