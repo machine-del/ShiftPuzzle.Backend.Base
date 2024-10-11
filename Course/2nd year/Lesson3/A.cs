1f9748d17db37522bda4a47d79d464f7827e6f46
machine <137556120+machine-del@users.noreply.github.com>
2024-10-11 18:08:15 +0500

    Практика А

---
+++ Course/2nd year/Lesson3/A.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+    }
+}
