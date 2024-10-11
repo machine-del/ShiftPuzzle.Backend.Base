0143b6ac0fab90ca1e6969402de54fc47bf589a4
machine <137556120+machine-del@users.noreply.github.com>
2024-10-11 18:09:48 +0500

    Практика С

---
+++ Course/2nd year/Lesson3/С.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 6
+        int[] numbers = new int[4];
+        // 7
+        int[] ints = new int[5] { 1, 2, 3, 4, 5 };
+        // 8
+        char[] chars = { 'A', 'B', 'C' };
+        // 9
+        int[] ints1 = { 1, 2, 3, 4, 5 };
+    }
+}
