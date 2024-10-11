a0b66b7d9db29a232a6ff996f143bcc268558ab8
machine <137556120+machine-del@users.noreply.github.com>
2024-10-11 18:08:56 +0500

    Практика В

---
+++ Course/2nd year/Lesson3/В.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{ 
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5};
+        Console.WriteLine(numbers[1]);
+        numbers[2] = 100;
+        Console.WriteLine(numbers[2]);
+        Console.WriteLine(numbers.Length);
+    }
+}
